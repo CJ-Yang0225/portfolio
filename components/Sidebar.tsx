5d8b7e65711600f2beef00d34455d2c9c05a6ac6
CJ-Yang0225 <57297482+CJ-Yang0225@users.noreply.github.com>
2021-03-22 16:42:48 +0800

    chore: update styles

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,10 +9,10 @@ const Sidebar = () => {
       <img
         src="https://lh3.googleusercontent.com/ogw/ADGmqu8FefabuNHnyjX9VFTGW-m9dvrFJOA40Lh3R7M=s192-c-mo"
         alt="User avatar"
-        className="w-32 h-32 mx-auto border-8 border-green-400 border-solid rounded-full"
+        className="w-32 h-32 mx-auto border-4 border-green-400 border-solid rounded-full"
       />
       <h3 className="my-4 text-3xl font-medium tracking-wide font-kaushan">
-        <span>Chao-Jie </span>
+        <span className="text-green">Chao-Jie </span>
         Yang
       </h3>
       <p className="px-2 py-1 my-3 bg-gray-200 rounded-full">
@@ -21,8 +21,7 @@ const Sidebar = () => {
       <a
         href=""
         download="name"
-        className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full"
-      >
+        className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full">
         <GiTie className="w-6 h-6" />
         <span>Download Resume</span>
       </a>
@@ -37,10 +36,12 @@ const Sidebar = () => {
           <AiFillGithub className="w-8 h-8" />
         </a>
       </div>
-      <div className="py-4 my-5 bg-gray-200">
+      <div
+        className="py-4 my-5 bg-gray-200"
+        style={{ marginLeft: "-1rem", marginRight: "-1rem" }}>
         <div className="flex items-center justify-center space-x-2">
           <GoLocation className="w-6 h-6" />
-          <span>Taichung,Taiwan</span>
+          <span>Taichung,TW</span>
         </div>
         <div className="flex items-center justify-center my-2 space-x-2">
           <AiOutlineMail className="w-6 h-6" />
@@ -52,10 +53,12 @@ const Sidebar = () => {
         </div>
       </div>
       <div className="flex flex-col items-center justify-center">
-        <button className="w-8/12 px-5 py-2 my-2 rounded-full bg-gradient-to-r from-green-400 to-blue-400">
+        <button
+          className="w-8/12 px-5 py-2 my-2 rounded-full bg-gradient-to-r from-green to-blue-400 focus:outline-none"
+          onClick={() => window.open("mailto:cj.yang0225@gmail.com")}>
           Email me
         </button>
-        <button className="w-8/12 px-5 py-2 my-2 rounded-full bg-gradient-to-r from-green-400 to-blue-400">
+        <button className="w-8/12 px-5 py-2 my-2 rounded-full bg-gradient-to-r from-green to-blue-400 focus:outline-none">
           Switch theme
         </button>
       </div>
